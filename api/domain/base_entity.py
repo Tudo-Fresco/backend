67a291e6f7cc6ff78e43a85e894f3c69834e4733
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-03-06 01:58:28 +0000

    Tests added for product name

--- api/domain/base_entity.py
+++ api/domain/base_entity.py
@@ -13,18 +13,26 @@ class BaseEntity(Base):
     Abstract base class that defines common fields for all models.
     It includes a UUID primary key, active status, and timestamps.
     """
-    __abstract__ = True  # Prevents SQLAlchemy from creating a table for this class
+    __abstract__ = True
 
     uuid = Column(UUID, primary_key=True, default=uuid4, unique=True, nullable=False)
     active = Column(Boolean, default=True, nullable=False)
-    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
     updated_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), onupdate=lambda: datetime.now(timezone.utc), nullable=False)
+    created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc), nullable=False)
 
     def __init__(self, **kwargs):
         """
         Constructor method to initialize attributes dynamically.
         """
-        super().__init__(**kwargs)  # Ensure SQLAlchemy handles attributes properly
+        if 'uuid' not in kwargs:
+            kwargs['uuid'] = uuid4()
+        if 'active' not in kwargs:
+            kwargs['active'] = True
+        if 'created_at' not in kwargs:
+            kwargs['created_at'] = datetime.now(timezone.utc)
+        if 'updated_at' not in kwargs:
+            kwargs['updated_at'] = datetime.now(timezone.utc)
+        super().__init__(**kwargs)
 
     @abstractmethod
     def validate(self):
@@ -38,15 +46,21 @@ class BaseEntity(Base):
 
     def to_dict(self) -> dict:
         result = {}
-        for key, value in self.__dict__.items():
+        properties: dict = self.__dict__.items()
+        for key, value in properties:
             if key.startswith('_'):
                 continue
-            if isinstance(value, UUID):
+            if isinstance(value, PythonUUID):
                 result[key] = str(value)
             elif isinstance(value, enum.Enum):
                 result[key] = value.value
-            # elif isinstance(value, BaseEntity):
-            #     result[key] = value.to_dict()
+            elif isinstance(value, BaseEntity):
+                if hasattr(value, 'to_dict'):
+                    result[key] = value.to_dict()
+                else:
+                    result[key] = str(value)
+            elif isinstance(value, datetime):
+                result[key] = value.isoformat()
             else:
                 result[key] = value
         return result
--- tests/entities/test_product_name.py
+++ tests/entities/test_product_name.py
@@ -1,6 +1,8 @@
+from unittest.mock import MagicMock, patch
 from api.exceptions.validation_exception import ValidationException
 from api.domain.product_name import ProductName
 from tests.helper.text_generator import TextGenerator
+from freezegun import freeze_time
 import unittest
 
 
@@ -26,3 +28,49 @@ class TestProductName(unittest.TestCase):
         with self.assertRaises(ValidationException) as context:
             product.validate()
         self.assertEqual('Nome do produto: Deve ser menor do que 256 caracters.\n', context.exception.message)
+        self.assertEqual(expected_product_name, product.name)
+    
+    @freeze_time('2025-03-06 12:00:00')
+    @patch('api.domain.base_entity.uuid4', return_value='123e4567-e89b-12d3-a456-426614174000', autospec=True)
+    def test_to_dict_must_convert_to_dict_correctly(self, mock_uuid: MagicMock) -> None:
+        expected_uuid = mock_uuid.return_value 
+        expected_product_name = 'Maçã'
+        expected_dict: dict = {
+            'name': expected_product_name,
+            'uuid': expected_uuid,
+            'created_at': '2025-03-06T12:00:00+00:00',
+            'updated_at': '2025-03-06T12:00:00+00:00',
+            'active': True
+        }
+        product = ProductName(name=expected_product_name)
+        result = product.to_dict()
+        self.assertDictEqual(expected_dict, result)
+        self.assertEqual(expected_product_name, product.name)
+
+    def test_active_must_activate_correctly(self) -> None:
+        instance_dict: dict = {
+            'name': 'Pimentão',
+            'uuid': '123e4567-e89b-12d3-a456-426614174000',
+            'created_at': '2025-03-06T12:00:00+00:00',
+            'updated_at': '2025-03-06T12:00:00+00:00',
+            'active': False
+        }
+        product_name = ProductName(**instance_dict)
+        self.assertFalse(product_name.active)
+        product_name.activate()
+        self.assertTrue(product_name.active)
+        self.assertNotEqual(instance_dict.get('updated_at'), product_name.updated_at.isoformat())
+
+    def test_dactivate_must_deactivate_correctly(self) -> None:
+        instance_dict: dict = {
+            'name': 'Pimentão',
+            'uuid': '123e4567-e89b-12d3-a456-426614174000',
+            'created_at': '2025-03-06T12:00:00+00:00',
+            'updated_at': '2025-03-06T12:00:00+00:00',
+            'active': True
+        }
+        product_name = ProductName(**instance_dict)
+        self.assertTrue(product_name.active)
+        product_name.deactivate()
+        self.assertFalse(product_name.active)
+        self.assertNotEqual(instance_dict.get('updated_at'), product_name.updated_at.isoformat())

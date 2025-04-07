566919673bf99c7dbdaa0bfb6c77ed753e00af34
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-04-07 00:52:07 +0000

    Alembic and .env.example added

---
+++ alembic/env.py
@@ -0,0 +1,50 @@
+import os
+import sys
+from logging.config import fileConfig
+from alembic import context
+from sqlalchemy import engine_from_config, pool
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from api.shared.env_variable_manager import EnvVariableManager
+from api.infrastructure.models.base_model import BaseModel
+
+env = EnvVariableManager()
+db_url = env.load('DB_URL', is_sensitive=True).string()
+
+config = context.config
+config.set_main_option("sqlalchemy.url", db_url)
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = BaseModel.metadata
+
+def run_migrations_offline():
+    context.configure(
+        url=db_url,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+def run_migrations_online():
+    connectable = engine_from_config(
+        config.get_section(config.config_ini_section),
+        prefix='sqlalchemy.',
+        poolclass=pool.NullPool,
+    )
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata
+        )
+        with context.begin_transaction():
+            context.run_migrations()
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()

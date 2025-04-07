47c1813ad74be05a8c8ecb1d598a7ac6516b6189
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-04-07 00:33:30 +0000

    Custom method create for store

--- api/controllers/models/store/store_request_model.py
+++ api/controllers/models/store/store_request_model.py
@@ -1,19 +1,18 @@
-from pydantic import EmailStr, Field
-from api.controllers.models.address.address_request_model import AddressRequestModel
-from api.controllers.models.base_request_model import BaseResquestModel
-from api.controllers.models.user.user_request_model import UserRequestModel
+from uuid import UUID
 from typing import List
+from pydantic import Field
+from api.controllers.models.base_request_model import BaseResquestModel
 
 
 class StoreRequestModel(BaseResquestModel):
-    images: List[str] = Field(default_factory=list, example=['https://example.com/image1.png'])
-    cnpj: str = Field(..., example='12.345.678/0001-99')
-    address: AddressRequestModel
-    reputation: float = Field(..., ge=0, le=5, example=4.7)
-    trade_name: str = Field(..., example='Loja Orgânica')
-    legal_name: str = Field(..., example='Loja Orgânica Ltda')
-    owner: UserRequestModel
-    legal_phone_contact: str = Field(..., example='(11) 1234-5678')
-    preferred_phone_contact: str = Field(..., example='(11) 98765-4321')
-    legal_email_contact: EmailStr = Field(..., example='legal@loja.com.br')
-    preferred_email_contact: EmailStr = Field(..., example='contato@loja.com.br')
+    cnpj: str = Field(..., example="12.345.678/0001-90", description="CNPJ da loja")
+    trade_name: str = Field(..., example="Tudo Fresco Orgânicos", description="Nome fantasia da loja")
+    legal_name: str = Field(..., example="Tudo Fresco Ltda", description="Razão social da loja")
+    legal_phone_contact: str = Field(..., example="+55 11 4002-8922", description="Telefone jurídico de contato")
+    preferred_phone_contact: str = Field(..., example="+55 11 98888-7777", description="Telefone preferencial de contato")
+    legal_email_contact: str = Field(..., example="juridico@tudofresco.com.br", description="E-mail jurídico da loja")
+    preferred_email_contact: str = Field(..., example="contato@tudofresco.com.br", description="E-mail preferencial da loja")
+    images: List[str] = Field(default_factory=list, example=["https://image1.com", "https://image2.com"], description="Lista de URLs de imagens da loja")
+    reputation: float = Field(0.0, example=4.5, description="Reputação da loja (de 0 a 5)")
+    owner_uuid: UUID = Field(..., example="6a79c74a-5016-4f6f-ae9c-83d3c6a30851", description="UUID do proprietário da loja")
+    address_uuid: UUID = Field(..., example="3ec37df2-2e6b-4b4a-9f25-60fd20ec0fd8", description="UUID do endereço da loja")
--- api/services/store_service.py
+++ api/services/store_service.py
@@ -1,10 +1,48 @@
+from http import HTTPStatus
 from api.controllers.models.store.store_request_model import StoreRequestModel
 from api.controllers.models.store.store_response_model import StoreResponseModel
 from api.domain.entities.store import Store
+from api.infrastructure.repositories.address_repository import AddressRepository
 from api.infrastructure.repositories.store_repository import StoreRepository
+from api.infrastructure.repositories.user_repository import UserRepository
 from api.services.base_service import BaseService
+from api.services.service_response import ServiceResponse
 
 
 class StoreService(BaseService[StoreRequestModel, StoreResponseModel, Store]):
-    def __init__(self, store_repository: StoreRepository):
+    def __init__(
+        self,
+        store_repository: StoreRepository,
+        user_repository: UserRepository,
+        address_repository: AddressRepository
+    ):
         super().__init__(store_repository, Store)
+        self.user_repo = user_repository
+        self.address_repo = address_repository
+
+    async def create(self, request: StoreRequestModel) -> ServiceResponse[StoreResponseModel]:
+        self.logger.log_info('Creating a Store')
+        owner = await self.user_repo.get(request.owner_uuid)
+        self._raise_not_found_when_none(owner, request.owner_uuid)
+        address = await self.address_repo.get(request.address_uuid)
+        self._raise_not_found_when_none(address, request.address_uuid)
+        store = Store(
+            cnpj=request.cnpj,
+            trade_name=request.trade_name,
+            legal_name=request.legal_name,
+            legal_phone_contact=request.legal_phone_contact,
+            preferred_phone_contact=request.preferred_phone_contact,
+            legal_email_contact=request.legal_email_contact,
+            preferred_email_contact=request.preferred_email_contact,
+            images=request.images,
+            reputation=request.reputation,
+            owner=owner,
+            address=address
+        )
+        created_id = await self.repository.create(store)
+        response = StoreResponseModel(**store.to_dict())
+        return ServiceResponse(
+            status=HTTPStatus.CREATED,
+            message=f'Loja {created_id} criada com sucesso',
+            payload=response
+        )

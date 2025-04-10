ac8670ddcb0c6db6c54a776c0ee883edf7d4a726
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-04-10 10:08:57 +0000

    BaseModels not being listed on the swagger documentaion

--- api/controllers/models/base_request_model.py
+++ api/controllers/models/base_request_model.py
@@ -1,4 +1,8 @@
+from typing import Any
 from pydantic import BaseModel
 
 class BaseRequestModel(BaseModel):
-    pass
+    class Config:
+        @staticmethod
+        def schema_extra(schema: dict[str, Any], model_class: type) -> None:
+            schema.clear()
--- api/controllers/models/base_response_model.py
+++ api/controllers/models/base_response_model.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Any
 from uuid import UUID
 from pydantic import BaseModel, Field
 
@@ -6,3 +7,8 @@ class BaseResponseModel(BaseModel):
     id: UUID = Field(..., example='de305d54-75b4-431b-adb2-eb6b9e546014')
     created_at: datetime = Field(..., example='2024-01-01T12:00:00Z')
     updated_at: datetime = Field(..., example='2024-01-01T12:30:00Z')
+
+    class Config:
+        @staticmethod
+        def schema_extra(schema: dict[str, Any], model_class: type) -> None:
+            schema.clear()

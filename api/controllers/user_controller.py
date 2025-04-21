0d43f645e0badae9d6a0462d8c5be24ca4aba121
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-04-21 00:31:50 +0000

    Sign up method implemented

--- api/controllers/user_controller.py
+++ api/controllers/user_controller.py
@@ -1,8 +1,12 @@
+from fastapi.responses import JSONResponse
 from api.controllers.auth_wrapper import AuthWrapper
 from api.controllers.models.user.user_request_model import UserRequestModel
 from api.controllers.models.user.user_response_model import UserResponseModel
 from api.services.i_service import IService
 from api.controllers.base_controller import BaseController
+from fastapi import APIRouter, Body, Depends, Query
+
+from api.services.service_response import ServiceResponse
 
 
 class UserController(BaseController[UserRequestModel, UserResponseModel]):
@@ -12,7 +16,22 @@ class UserController(BaseController[UserRequestModel, UserResponseModel]):
             service=service,
             request_model=UserRequestModel,
             response_model=UserResponseModel,
-            prefix="/user",
+            prefix='/user',
             tag=self.__class__.__name__,
             auth_wrapper=auth_wrapper
         )
+        self.router.add_api_route(
+            path='/sign-up',
+            endpoint=self._sign_up_handler(),
+            methods=['POST'],
+            response_model=UserResponseModel,
+            status_code=201,
+            summary=f'Sign Up {self.__class__.__name__}',
+        )
+
+    def _sign_up_handler(self):
+        async def sign_up(model: UserRequestModel = Body(...)) -> JSONResponse:
+            self.logger.log_info(f'Creating a new user, e-mail: {model.email}')
+            service_response: ServiceResponse = await self.service.sign_up(request=model)
+            return self.make_response(service_response)
+        return sign_up
--- api/services/user_service.py
+++ api/services/user_service.py
@@ -1,6 +1,8 @@
 from api.controllers.models.user.user_request_model import UserRequestModel
 from api.controllers.models.user.user_response_model import UserResponseModel
 from api.domain.entities.user import User
+from api.enums.user_access import UserAccess
+from api.exceptions.validation_exception import ValidationException
 from api.infrastructure.repositories.user_repository import UserRepository
 from api.services.base_service import BaseService
 from api.services.service_exception_catcher import ServiceExceptionCatcher
@@ -28,3 +30,10 @@ class UserService(BaseService[UserRequestModel, UserResponseModel, User]):
     async def get_by_email(self, email: str) -> ServiceResponse[User]:
         user: User = await self.repository.get_by_email(email)
         return ServiceResponse(status=HTTPStatus.OK, message=f'O registro {email} foi encontrado com sucesso', payload=user)
+    
+    @catch
+    async def sign_up(self, request: UserRequestModel) -> ServiceResponse[UserResponseModel]:
+        if request.user_access == UserAccess.ADMIN:
+            raise ValidationException('Não é possível cadastrar-se como administrador, escolha outra opção.')
+        service_response = await self.create(request)
+        return service_response

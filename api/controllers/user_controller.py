87940983b430e1bd2a535e8f4392dc77b316a711
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-04-24 01:11:26 +0000

    User controller sorted

--- api/controllers/user_controller.py
+++ api/controllers/user_controller.py
@@ -2,9 +2,10 @@ from fastapi.responses import JSONResponse
 from api.controllers.auth_wrapper import AuthWrapper
 from api.controllers.models.user.user_request_model import UserRequestModel
 from api.controllers.models.user.user_response_model import UserResponseModel
+from api.enums.user_access import UserAccess
 from api.services.i_service import IService
 from api.controllers.base_controller import BaseController
-from fastapi import APIRouter, Body, Depends, Query
+from fastapi import Body, Depends, File, UploadFile
 
 from api.services.service_response import ServiceResponse
 
@@ -28,6 +29,15 @@ class UserController(BaseController[UserRequestModel, UserResponseModel]):
             status_code=201,
             summary=f'Sign Up {self.__class__.__name__}',
         )
+        self.router.add_api_route(
+            path='/profile-picture',
+            endpoint=self._upload_profile_picture_handler(),
+            methods=['POST'],
+            response_model=UserResponseModel,
+            status_code=200,
+            summary=f'Upload Profile Picture for {self.__class__.__name__}',
+            dependencies=[Depends(self.auth_wrapper.with_access([UserAccess.ADMIN, UserAccess.STORE_OWNER]))]
+        )
 
     def _sign_up_handler(self):
         async def sign_up(model: UserRequestModel = Body(...)) -> JSONResponse:
@@ -35,3 +45,20 @@ class UserController(BaseController[UserRequestModel, UserResponseModel]):
             service_response: ServiceResponse = await self.service.sign_up(request=model)
             return self.make_response(service_response)
         return sign_up
+    
+
+    def _upload_profile_picture_handler(self):
+            async def upload_profile_picture(
+                file: UploadFile = File(...),
+                user: UserResponseModel = Depends(self.auth_wrapper.with_access([UserAccess.STORE_OWNER, UserAccess.ADMIN]))
+            ) -> JSONResponse:
+                self.logger.log_info(f'Handling profile picture upload for user {user.uuid}')
+                image_bytes = await file.read()
+                file_name = file.filename
+                service_response: ServiceResponse = await self.service.upload_profile_picture(
+                    user_uuid=user.uuid,
+                    image_bytes=image_bytes,
+                    file_name=file_name
+                )
+                return self.make_response(service_response)
+            return upload_profile_picture

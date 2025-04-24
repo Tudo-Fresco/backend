5f4c9b35c082b0b8845fdf5974d9235c6b1ba0d1
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-04-24 01:11:43 +0000

    Real cloud bucket names

--- api/enums/bucket_name.py
+++ api/enums/bucket_name.py
@@ -3,5 +3,5 @@ from enum import Enum
 
 class BucketName(str, Enum):
     USER_PROFILE = 'user-profiles-images'
-    PRODUCT_IMAGES = 'product-images'
-    STORE_PICTURES = 'store-images'
+    PRODUCT_IMAGES = 'product-portfolio-images'
+    STORE_PICTURES = 'store-catalog-images'

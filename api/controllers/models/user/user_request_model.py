77ba676c90c6216a511392da2a9e2e5bdf97e5d8
Gabriel Arno Voltolini <gbrl.volt@gmail.com>
2025-04-27 18:39:16 +0000

    Email validation removed from pydantic

--- api/controllers/models/user/user_request_model.py
+++ api/controllers/models/user/user_request_model.py
@@ -1,5 +1,5 @@
 from datetime import date
-from pydantic import EmailStr, Field
+from pydantic import Field
 from api.controllers.models.base_request_model import BaseRequestModel
 from api.enums.gender_type import GenderType
 from api.enums.user_access import UserAccess
@@ -7,7 +7,7 @@ from api.enums.user_access import UserAccess
 
 class UserRequestModel(BaseRequestModel):
     name: str = Field(..., example='Gabriel Voltolini')
-    email: EmailStr = Field(..., example='gabriel@example.com')
+    email: str = Field(..., example='gabriel@example.com')
     date_of_birth: date = Field(..., example='2000-01-01')
     gender: GenderType = Field(..., example=GenderType.MALE.value)
     phone_number: str = Field(..., example='+55 47 99999-9999')
